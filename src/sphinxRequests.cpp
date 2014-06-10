676f1b5bb61a3782fea5aa80bc2b10d0658f357e
alex <wvdial@gmail.com>
2014-06-10 13:33:24 +0300

    logging

--- src/sphinxRequests.cpp
+++ src/sphinxRequests.cpp
@@ -22,10 +22,9 @@ std::string sphinxRequests::getBranchName() const
     switch(branches)
     {
         case EBranchT::T1:  return "user search";
-        case EBranchT::T2: return "context";
+        case EBranchT::T2: return "page context";
         case EBranchT::T3: return "short term history";
-        case EBranchT::T4: return "short long history";
-        case EBranchT::T5: return "retargeting";
+        case EBranchT::T4: return "long term history";
         default : return "";
     }
 }
