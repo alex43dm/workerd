cb0d51483b0211a0608c49cc540f82d6294f9fff
alex <wvdial@gmail.com>
2014-06-11 09:45:03 +0300

    filter

--- src/ParentDB.cpp
+++ src/ParentDB.cpp
@@ -977,7 +977,7 @@ void ParentDB::CampaignLoad(const std::string &aCampaignId)
             {
                 bzero(buf,sizeof(buf));
                 sqlite3_snprintf(sizeof(buf),buf,
-                                 "INSERT INTO Campaign2Accounts(id_cam,id_acc,allowed) VALUES(%lld,1,0);",
+                                 "INSERT INTO Campaign2Accounts(id_cam,id_acc,allowed) VALUES(%lld,1,1);",
                                  long_id
                                 );
                 try
