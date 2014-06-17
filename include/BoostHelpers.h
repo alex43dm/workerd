3b143455c48e722a851558baf7e329fc12985392
alex <wvdial@gmail.com>
2014-06-17 10:06:58 +0300

    a) save config b) new message queue commands c) new parametr offer_by_campaign_unique d) new algorithm in ris

---
+++ include/BoostHelpers.h
@@ -0,0 +1,20 @@
+#ifndef BOOSTHELPERS_H
+#define BOOSTHELPERS_H
+
+#include <string>
+
+class BoostHelpers
+{
+    public:
+        BoostHelpers();
+        virtual ~BoostHelpers();
+
+        static std::string getConfigDir(const std::string &filePath);
+        static bool checkPath(const std::string &path_, bool checkWrite, bool isFile);
+        static int getSeconds(const std::string &s);
+        static std::string float2string(const float);
+    protected:
+    private:
+};
+
+#endif // BOOSTHELPERS_H
