9a0819d598451a64a950ab72b8025bef10cd3167
alex <wvdial@gmail.com>
2014-07-03 12:02:13 +0300

    campaign allowed fix

--- db_dump/tables/07_Campaign2Accounts.sql
+++ db_dump/tables/07_Campaign2Accounts.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS Campaign2Accounts
 id_cam INT8 NOT NULL,
 id_acc INT8 NOT NULL,
 allowed SMALLINT,
-UNIQUE (id_cam,id_acc,allowed) ON CONFLICT IGNORE,
+UNIQUE (id_cam,id_acc) ON CONFLICT IGNORE,
 FOREIGN KEY(id_cam) REFERENCES Campaign(id),
 FOREIGN KEY(id_acc) REFERENCES Accounts(id)
 );
