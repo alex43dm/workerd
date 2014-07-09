52df13aac22c3e0d1d868ac86a683acca29d275c
alex <wvdial@gmail.com>
2014-07-09 10:02:37 +0300

    new retargeting

--- db_dump/requests/03.sql
+++ db_dump/requests/03.sql
@@ -22,11 +22,11 @@ ca.guid,
 ca.offer_by_campaign_unique
 FROM Offer AS ofrs INDEXED BY idx_Offer_id
 INNER JOIN Campaign AS ca INDEXED BY idx_Campaign_id ON ca.valid=1 AND ca.retargeting=1 AND ofrs.campaignId=ca.id
-LEFT JOIN Session AS ses INDEXED BY idx_Session_id_offerId ON ofrs.id=ses.offerId AND ses.id=%llu AND ses.uniqueHits <= 0 AND ses.tail=0
---LEFT JOIN Retargeting AS ret1 INDEXED BY idx_Retargeting_offerId ON ofrs.id = ret1.offerId
+LEFT JOIN Session AS ses INDEXED BY idx_Session_id_offerId ON ofrs.id=ses.offerId
+    AND ses.id=%llu AND ses.tail=0
 WHERE ofrs.id IN(%s)
-	AND ofrs.valid=1
-	AND ses.offerId IS NULL
---ORDER BY ret1.viewTime ASC
+	AND ofrs.valid = 1
+	AND ses.uniqueHits > 0
+ORDER BY ret1.viewTime ASC
 --LIMIT 100
 ;
